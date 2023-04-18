9c0ac19cf22a8f7b77628e8c42af2565a0fa000a
Debajyoti Shit <ds2250@it.jgec.ac.in>
2023-04-18 19:21:26 +0530

    1st commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { IoMdFlashlight } from 'react-icons/io';
+export default function Header() {
+  return (
+    <header>
+      <h1>
+        <IoMdFlashlight />
+        Notes Keeper
+      </h1>
+    </header>
+  );
+}
+
+//export default Header;
